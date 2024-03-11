31be2f30d256754eb4555afd6bed88d30b3b6908
Muhammad Hamizi Jaminan <hello@hamizi.net>
2024-03-11 20:47:17 +0800

    Features: Directory User Alias

--- src/Directory/UserAlias.php
+++ src/Directory/UserAlias.php
@@ -6,4 +6,47 @@ use Hymns\GoogleAdminConsole\Directory;
 
 class UserAlias extends Directory
 {
+    /**
+     * Lists all aliases for a user.
+     *
+     * @link https://developers.google.com/admin-sdk/directory/reference/rest/v1/users.aliases/list
+     * @param string $userId
+     * @return array
+     */
+    public function list(string $userId)
+    {
+        return $this->client->request('GET', 'users/' . $userId . '/aliases');
+    }
+
+    /**
+     * Adds an alias.
+     *
+     * @link https://developers.google.com/admin-sdk/directory/reference/rest/v1/users.aliases/insert
+     * @param string $userId
+     * @param string $primaryEmail
+     * @param string $alias
+     * @return array
+     */
+    public function create(string $userId, string $primaryEmail, string $alias)
+    {
+        $body = [
+            'primaryEmail' => $primaryEmail,
+            'alias' => $alias
+        ];
+
+        return $this->client->request('POST', 'users/' . $userId . '/aliases', $body);
+    }
+
+    /**
+     * Removes an alias.
+     *
+     * @link https://developers.google.com/admin-sdk/directory/reference/rest/v1/users.aliases/delete
+     * @param string $userId
+     * @param string $alias
+     * @return array
+     */
+    public function delete(string $userId, string $alias)
+    {
+        return $this->client->request('DELETE', 'users/' . $userId. '/aliases/' . $alias);
+    }
 }

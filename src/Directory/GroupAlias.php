b823c3b4222b0d34c70eb77ca78ebc0e28772acf
Muhammad Hamizi Jaminan <hello@hamizi.net>
2024-03-11 20:39:49 +0800

    Features: Directory Group Alias

--- src/Directory/GroupAlias.php
+++ src/Directory/GroupAlias.php
@@ -4,7 +4,49 @@ namespace Hymns\GoogleAdminConsole\Directory;
 
 use Hymns\GoogleAdminConsole\Directory;
 
-
 class GroupAlias extends Directory
 {
+    /**
+     * Lists all aliases for a group.
+     *
+     * @link https://developers.google.com/admin-sdk/directory/reference/rest/v1/groups.aliases/list
+     * @param string $groupId
+     * @return array
+     */
+    public function list(string $groupId)
+    {
+        return $this->client->request('GET', 'groups/' . $groupId . '/aliases');
+    }
+
+    /**
+     * Adds an alias for the group.
+     *
+     * @link https://developers.google.com/admin-sdk/directory/reference/rest/v1/groups.aliases/insert
+     * @param string $groupId
+     * @param string $primaryEmail
+     * @param string $alias
+     * @return array
+     */
+    public function create(string $groupId, string $primaryEmail, string $alias)
+    {
+        $body = [
+            'primaryEmail' => $primaryEmail,
+            'alias' => $alias
+        ];
+
+        return $this->client->request('POST', 'groups/' . $groupId . '/domainaliases', $body);
+    }
+
+    /**
+     * Removes an alias.
+     *
+     * @link https://developers.google.com/admin-sdk/directory/reference/rest/v1/groups.aliases/delete
+     * @param string $groupId
+     * @param string $alias
+     * @return array
+     */
+    public function delete(string $groupId, string $alias)
+    {
+        return $this->client->request('DELETE', 'groups/' . $groupId. '/domainaliases/' . $alias);
+    }
 }
